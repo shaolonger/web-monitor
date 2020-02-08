4f7051db2ce72337d0c7add4440ce893a98d285a
shaolonger <shaolong.chuang@qq.com>
2020-02-08 09:04:41 +0800

    [update]mysql: 增加monitor-platform.sql

---
+++ 3.sql/monitor-platform.sql
@@ -0,0 +1,154 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     2020/2/8 8:55:53                             */
+/*==============================================================*/
+
+
+drop table if exists lms_custom_error_log;
+
+drop table if exists lms_http_error_log;
+
+drop table if exists lms_js_error_log;
+
+drop table if exists lms_resource_load_error_log;
+
+drop table if exists pms_project;
+
+drop table if exists ums_user;
+
+/*==============================================================*/
+/* Table: lms_custom_error_log                                  */
+/*==============================================================*/
+create table lms_custom_error_log
+(
+   id                   bigint not null,
+   log_type             varchar(50) not null,
+   create_time          datetime not null,
+   user_id              bigint not null,
+   user_name            varchar(64),
+   page_url             text,
+   page_key             varchar(50),
+   device_name          varchar(100),
+   os                   varchar(20),
+   browser_name         varchar(20),
+   browser_version      text,
+   ip_address           varchar(50),
+   error_type           varchar(30) not null,
+   error_message        text not null,
+   primary key (id)
+);
+
+alter table lms_custom_error_log comment '自定义异常日志表';
+
+/*==============================================================*/
+/* Table: lms_http_error_log                                    */
+/*==============================================================*/
+create table lms_http_error_log
+(
+   id                   bigint not null comment '主键',
+   log_type             varchar(50) not null comment '日志类型',
+   create_time          datetime not null,
+   user_id              bigint not null,
+   user_name            varchar(64),
+   page_url             text,
+   page_key             varchar(50),
+   device_name          varchar(100),
+   os                   varchar(20),
+   browser_name         varchar(20),
+   browser_version      text,
+   ip_address           varchar(50),
+   http_type            int(1) comment 'http请求类型，如"request"、"response"',
+   http_url_complete    text not null comment '完整的http请求地址',
+   http_url_short       text not null,
+   status               varchar(20) not null,
+   status_text          varchar(50),
+   res_time             varchar(13),
+   primary key (id)
+);
+
+alter table lms_http_error_log comment 'HTTP异常日志表';
+
+/*==============================================================*/
+/* Table: lms_js_error_log                                      */
+/*==============================================================*/
+create table lms_js_error_log
+(
+   id                   bigint not null,
+   log_type             varchar(50) not null,
+   create_time          datetime not null,
+   user_id              bigint not null,
+   user_name            varchar(64),
+   page_url             text,
+   page_key             varchar(50),
+   device_name          varchar(100),
+   os                   varchar(20),
+   browser_name         varchar(20),
+   browser_version      text,
+   ip_address           varchar(50),
+   error_type           varchar(30) not null,
+   error_message        text not null,
+   error_stack          text,
+   primary key (id)
+);
+
+alter table lms_js_error_log comment 'JS异常日志表';
+
+/*==============================================================*/
+/* Table: lms_resource_load_error_log                           */
+/*==============================================================*/
+create table lms_resource_load_error_log
+(
+   id                   bigint not null comment '主键',
+   log_type             varchar(50) not null comment '日志类型',
+   create_time          datetime not null,
+   user_id              bigint not null,
+   user_name            varchar(64),
+   page_url             text,
+   page_key             varchar(50),
+   device_name          varchar(100),
+   os                   varchar(20),
+   browser_name         varchar(20),
+   browser_version      text,
+   ip_address           varchar(50),
+   resource_url         text not null,
+   resource_type        varchar(20) not null,
+   status               int(1) not null,
+   primary key (id)
+);
+
+alter table lms_resource_load_error_log comment '资源加载异常日志表';
+
+/*==============================================================*/
+/* Table: pms_project                                           */
+/*==============================================================*/
+create table pms_project
+(
+   id                   bigint not null,
+   project_name         varchar(100) not null,
+   description          varchar(200),
+   create_time          datetime,
+   update_time          datetime,
+   primary key (id)
+);
+
+alter table pms_project comment '项目表';
+
+/*==============================================================*/
+/* Table: ums_user                                              */
+/*==============================================================*/
+create table ums_user
+(
+   id                   bigint not null,
+   username             varchar(64) not null,
+   password             varchar(64) not null,
+   phone                varchar(64),
+   icon                 varchar(500),
+   gender               int(1),
+   email                varchar(100),
+   create_time          datetime,
+   update_time          datetime,
+   primary key (id)
+);
+
+alter table ums_user comment '用户表';
+
