e6bb23a5cc0a15977a8fb8a7d0f9e6c4ed6740d0
shaolonger <shaolong.chuang@qq.com>
2020-02-08 14:20:59 +0800

    [update]server->monitor-platform：初始化server springboot项目

---
+++ 4.monitor-platform/src/test/java/com/shaolonger/monitorplatform/MonitorPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaolonger.monitorplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
