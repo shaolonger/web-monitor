1a2b9dbf11468c86ef27a1fb1910f3876c301b49
shaolonger <shaolong.chuang@qq.com>
2020-03-01 18:23:28 +0800

    [update]web：完成404页面

--- 5.web/src/modules/NotFound/index.js
+++ 5.web/src/modules/NotFound/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+// 组件
+import {Result} from 'antd';
+
+// css
+import './index.scss';
+
 const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <div className='notFound-container'>
+            <Result
+                status="404"
+                title="404"
+                subTitle="抱歉，找不到该页面"
+            />
+        </div>
     );
 };
 
