95767a56235e146f8454f9fd3f57581cc42d3bb0
shaolonger <shaolong.chuang@qq.com>
2020-03-03 08:58:10 +0800

    [update]web：更改antd国际化配置，设置为中文，完成用户注册审核页部分内容

--- 5.web/src/modules/Main/children/SystemManage/index.js
+++ 5.web/src/modules/Main/children/SystemManage/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {DatePicker, Select} from 'antd';
 
 // css
 import './index.scss';
 
 const SystemManage = () => {
     return (
-        <div className='systemManage-container'>用户注册审核页</div>
+        <div className='systemManage-container'>
+            <div className='systemManage-topBanner'>
+                <div className='systemManage-topBanner-datePicker'>
+                    <DatePicker.RangePicker showTime/>
+                </div>
+                <div className='systemManage-topBanner-radio'>
+                    <Select defaultValue="-1" style={{width: 120}}
+                            onChange={value => console.log(`selected ${value}`)}>
+                        <Select.Option value="-1">不限</Select.Option>
+                        <Select.Option value="0">未审核</Select.Option>
+                        <Select.Option value="1">审核通过</Select.Option>
+                        <Select.Option value="2">审核不通过</Select.Option>
+                    </Select>
+                </div>
+            </div>
+        </div>
     );
 };
 
