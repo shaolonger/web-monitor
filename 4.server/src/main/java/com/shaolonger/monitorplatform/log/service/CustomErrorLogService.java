a286cb9c78b5f6a19b70ceae439dc0c4958aa4cd
shaolonger <shaolong.chuang@qq.com>
2020-09-20 19:15:33 +0800

    fix(CustomErrorLogService): 修复get方法中sql拼接错误的bug

--- 4.server/src/main/java/com/shaolonger/monitorplatform/log/service/CustomErrorLogService.java
+++ 4.server/src/main/java/com/shaolonger/monitorplatform/log/service/CustomErrorLogService.java
@@ -55,6 +55,89 @@ public class CustomErrorLogService extends ServiceBase {
         Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
         Map<String, Object> paramMap = new HashMap<>();
         StringBuilder dataSqlBuilder = new StringBuilder("select * from lms_custom_error_log t where 1=1");
+        StringBuilder countSqlBuilder = new StringBuilder("select count(t.id) from lms_custom_error_log t where 1=1");
+        StringBuilder paramSqlBuilder = new StringBuilder();
+
+        // 项目标识
+        if (projectIdentifier != null && !projectIdentifier.isEmpty()) {
+            paramSqlBuilder.append(" and t.project_identifier = :projectIdentifier");
+            paramMap.put("projectIdentifier", projectIdentifier);
+        }
+        // 日志类型
+        if (logType != null && !logType.isEmpty()) {
+            paramSqlBuilder.append(" and t.log_type like :logType");
+            paramMap.put("logType", "%" + logType + "%");
+        }
+        // 开始时间、结束时间
+        if (startTime != null && endTime != null) {
+            paramSqlBuilder.append(" and t.create_time between :startTime and :endTime");
+            paramMap.put("startTime", startTime);
+            paramMap.put("endTime", endTime);
+        } else if (startTime != null) {
+            paramSqlBuilder.append(" and t.create_time >= :startTime");
+            paramMap.put("startTime", startTime);
+        } else if (endTime != null) {
+            paramSqlBuilder.append(" and t.create_time <= :endTime");
+            paramMap.put("endTime", endTime);
+        }
+        // 用户名
+        if (userName != null && !userName.isEmpty()) {
+            paramSqlBuilder.append(" and t.user_name like :userName");
+            paramMap.put("userName", "%" + userName + "%");
+        }
+        // 页面URL
+        if (pageUrl != null && !pageUrl.isEmpty()) {
+            paramSqlBuilder.append(" and t.page_url like :pageUrl");
+            paramMap.put("pageUrl", "%" + pageUrl + "%");
+        }
+        // JS错误类型
+        if (errorType != null && !errorType.isEmpty()) {
+            paramSqlBuilder.append(" and t.error_type like :errorType");
+            paramMap.put("errorType", "%" + errorType + "%");
+        }
+        // JS错误信息
+        if (errorMessage != null && !errorMessage.isEmpty()) {
+            paramSqlBuilder.append(" and t.error_message like :errorMessage");
+            paramMap.put("errorMessage", "%" + errorMessage + "%");
+        }
+        dataSqlBuilder.append(paramSqlBuilder).append(" order by t.create_time desc");
+        countSqlBuilder.append(paramSqlBuilder);
+        Page<CustomErrorLog> page = this.findPageBySqlAndParam(CustomErrorLog.class, dataSqlBuilder.toString(), countSqlBuilder.toString(), pageable, paramMap);
+
+        // 返回
+        PageResultBase<CustomErrorLog> pageResultBase = new PageResultBase<>();
+        pageResultBase.setTotalNum(page.getTotalElements());
+        pageResultBase.setTotalPage(page.getTotalPages());
+        pageResultBase.setPageNum(pageNum);
+        pageResultBase.setPageSize(pageSize);
+        pageResultBase.setRecords(page.getContent());
+        return pageResultBase;
+    }
+
+    /**
+     * 条件查询
+     *
+     * @param request request
+     * @return Object
+     */
+    public Object getByGroup(HttpServletRequest request) {
+        // 获取请求参数
+        int pageNum = DataConvertUtils.strToInt(request.getParameter("pageNum"));
+        int pageSize = DataConvertUtils.strToInt(request.getParameter("pageSize"));
+        String projectIdentifier = request.getParameter("projectIdentifier");
+        String logType = request.getParameter("logType");
+        Date startTime = DateUtils.strToDate(request.getParameter("startTime"), "yyyy-MM-dd HH:mm:ss");
+        Date endTime = DateUtils.strToDate(request.getParameter("endTime"), "yyyy-MM-dd HH:mm:ss");
+        String userName = request.getParameter("userName");
+        String pageUrl = request.getParameter("pageUrl");
+        String errorType = request.getParameter("errorType");
+        String errorMessage = request.getParameter("errorMessage");
+
+        // 拼接sql，分页查询
+        Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
+        Map<String, Object> paramMap = new HashMap<>();
+        StringBuilder dataSqlBuilder = new StringBuilder("select count(t.id) as count, max(t.create_time) as latest_create_time, " +
+                "count(distinct t.user_id) as user_count, t.error_message from lms_custom_error_log t where 1=1");
         StringBuilder countSqlBuilder = new StringBuilder("select count(*) from (select count(t.id) as count, max(t.create_time) " +
                 "as latest_create_time, count(t.user_id) as user_count, t.error_message from lms_custom_error_log t where 1=1");
         StringBuilder paramSqlBuilder = new StringBuilder();
@@ -101,91 +184,8 @@ public class CustomErrorLogService extends ServiceBase {
             paramSqlBuilder.append(" and t.error_message like :errorMessage");
             paramMap.put("errorMessage", "%" + errorMessage + "%");
         }
-        dataSqlBuilder.append(paramSqlBuilder).append(" order by t.create_time desc");
-        countSqlBuilder.append(paramSqlBuilder.append(" group by t.error_message) s"));
-        Page<CustomErrorLog> page = this.findPageBySqlAndParam(CustomErrorLog.class, dataSqlBuilder.toString(), countSqlBuilder.toString(), pageable, paramMap);
-
-        // 返回
-        PageResultBase<CustomErrorLog> pageResultBase = new PageResultBase<>();
-        pageResultBase.setTotalNum(page.getTotalElements());
-        pageResultBase.setTotalPage(page.getTotalPages());
-        pageResultBase.setPageNum(pageNum);
-        pageResultBase.setPageSize(pageSize);
-        pageResultBase.setRecords(page.getContent());
-        return pageResultBase;
-    }
-
-    /**
-     * 条件查询
-     *
-     * @param request request
-     * @return Object
-     */
-    public Object getByGroup(HttpServletRequest request) {
-        // 获取请求参数
-        int pageNum = DataConvertUtils.strToInt(request.getParameter("pageNum"));
-        int pageSize = DataConvertUtils.strToInt(request.getParameter("pageSize"));
-        String projectIdentifier = request.getParameter("projectIdentifier");
-        String logType = request.getParameter("logType");
-        Date startTime = DateUtils.strToDate(request.getParameter("startTime"), "yyyy-MM-dd HH:mm:ss");
-        Date endTime = DateUtils.strToDate(request.getParameter("endTime"), "yyyy-MM-dd HH:mm:ss");
-        String userName = request.getParameter("userName");
-        String pageUrl = request.getParameter("pageUrl");
-        String errorType = request.getParameter("errorType");
-        String errorMessage = request.getParameter("errorMessage");
-
-        // 拼接sql，分页查询
-        Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
-        Map<String, Object> paramMap = new HashMap<>();
-        StringBuilder dataSqlBuilder = new StringBuilder("select count(t.id) as count, max(t.create_time) as latest_create_time, " +
-                "count(distinct t.user_id) as user_count, t.error_message from lms_custom_error_log t where 1=1");
-        StringBuilder countSqlBuilder = new StringBuilder("select count(t.id) from lms_custom_error_log t where 1=1");
-        StringBuilder paramSqlBuilder = new StringBuilder();
-
-        // 项目标识
-        if (projectIdentifier != null && !projectIdentifier.isEmpty()) {
-            paramSqlBuilder.append(" and t.project_identifier = :projectIdentifier");
-            paramMap.put("projectIdentifier", projectIdentifier);
-        }
-        // 日志类型
-        if (logType != null && !logType.isEmpty()) {
-            paramSqlBuilder.append(" and t.log_type like :logType");
-            paramMap.put("logType", "%" + logType + "%");
-        }
-        // 开始时间、结束时间
-        if (startTime != null && endTime != null) {
-            paramSqlBuilder.append(" and t.create_time between :startTime and :endTime");
-            paramMap.put("startTime", startTime);
-            paramMap.put("endTime", endTime);
-        } else if (startTime != null) {
-            paramSqlBuilder.append(" and t.create_time >= :startTime");
-            paramMap.put("startTime", startTime);
-        } else if (endTime != null) {
-            paramSqlBuilder.append(" and t.create_time <= :endTime");
-            paramMap.put("endTime", endTime);
-        }
-        // 用户名
-        if (userName != null && !userName.isEmpty()) {
-            paramSqlBuilder.append(" and t.user_name like :userName");
-            paramMap.put("userName", "%" + userName + "%");
-        }
-        // 页面URL
-        if (pageUrl != null && !pageUrl.isEmpty()) {
-            paramSqlBuilder.append(" and t.page_url like :pageUrl");
-            paramMap.put("pageUrl", "%" + pageUrl + "%");
-        }
-        // JS错误类型
-        if (errorType != null && !errorType.isEmpty()) {
-            paramSqlBuilder.append(" and t.error_type like :errorType");
-            paramMap.put("errorType", "%" + errorType + "%");
-        }
-        // JS错误信息
-        if (errorMessage != null && !errorMessage.isEmpty()) {
-            paramSqlBuilder.append(" and t.error_message like :errorMessage");
-            paramMap.put("errorMessage", "%" + errorMessage + "%");
-        }
         dataSqlBuilder.append(paramSqlBuilder).append(" group by t.error_message order by count desc");
-        countSqlBuilder.append(paramSqlBuilder);
+        countSqlBuilder.append(paramSqlBuilder.append(" group by t.error_message) s"));
         Page page = this.findPageByNativeSqlAndParam(dataSqlBuilder.toString(), countSqlBuilder.toString(), pageable, paramMap);
 
         // 转换vo
