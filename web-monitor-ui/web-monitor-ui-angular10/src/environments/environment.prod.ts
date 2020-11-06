26f79512f05f5f1e5e75dfa59a487a1575329b27
shaolonger <shaolong.chuang@qq.com>
2020-11-07 01:03:51 +0800

    feat: 调整jsSdkAPIBasicUrl

--- web-monitor-ui/web-monitor-ui-angular10/src/environments/environment.prod.ts
+++ web-monitor-ui/web-monitor-ui-angular10/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiBasicUrl: '/api', // 后台地址【上线前需修改此处】
-    jsSdkAPIBasicUrl: 'http://api.com/jsSdk/get?v=1.0.0&k=' // 通过script标签引用js sdk包的地址【上线前需修改此处】
+    jsSdkAPIBasicUrl: 'http://api.com/jsSdk/jsSdk.js&key=' // 通过script标签引用js sdk包的地址【上线前需修改此处】
 };
