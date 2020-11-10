d14bfcb00540e68824c6e4818bbfb4fb8d3154cc
shaolonger <shaolong.chuang@qq.com>
2020-11-10 19:27:17 +0800

    feat(web-monitor-server): TokenService，token有效期从10分钟调整为60分钟

--- web-monitor-server/web-monitor-server-java/src/main/java/com/shaolonger/webmonitorserver/auth/service/TokenService.java
+++ web-monitor-server/web-monitor-server-java/src/main/java/com/shaolonger/webmonitorserver/auth/service/TokenService.java
@@ -20,7 +20,7 @@ public class TokenService {
      * token在redis中的有效时间
      * 单位：分钟
      */
-    public static final int REDIS_TOKEN_EXPIRE = 10;
+    public static final int REDIS_TOKEN_EXPIRE = 60;
 
     /**
      * 判断token是否存在
