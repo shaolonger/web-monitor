72a479cb77ad9e9d6fd503cf4adb02ff9bd8d2b0
shaolonger <shaolong.chuang@qq.com>
2021-01-29 01:28:55 +0800

    feat(web-monitor-ui-flutter): 通过环境变量动态设置API_URL

--- web-monitor-ui/web-monitor-ui-flutter/lib/config/environment_config.dart
+++ web-monitor-ui/web-monitor-ui-flutter/lib/config/environment_config.dart
@@ -1,7 +1,11 @@
 class EnvironmentConfig {
-  /// ENV为运行环境，包括dev-开发环境，test-测试环境，prod-生产环境
+  // ENV为运行环境，包括dev-开发环境，test-测试环境，prod-生产环境
   static const ENV = String.fromEnvironment(
     "ENV",
     defaultValue: "dev",
   );
+
+  // URL前缀地址
+  static const String API_URL =
+      ENV == "dev" ? "http://localhost:6001" : "http://localhost:6001";
 }
