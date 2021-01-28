e1b2989cbebc2c7104607516ec9f0575070d4856
shaolonger <shaolong.chuang@qq.com>
2021-01-29 01:30:04 +0800

    feat(web-monitor-ui-flutter): 因在main方法中调用async函数，因此需要先运行WidgetsFlutterBinding.ensureInitialized();

--- web-monitor-ui/web-monitor-ui-flutter/lib/main.dart
+++ web-monitor-ui/web-monitor-ui-flutter/lib/main.dart
@@ -2,4 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:web_monitor_app/common/Global.dart';
 import 'package:web_monitor_app/modules/module_main/module_main.dart';
 
-void main() => Global.init().then((value) => runApp(ModuleMain()));
+void main() {
+  // 注意，在main里调用async函数需要首先执行下面这行
+  // 参考文章：https://stackoverflow.com/questions/57689492/flutter-unhandled-exception-servicesbinding-defaultbinarymessenger-was-accesse
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Global.init().then((value) => runApp(ModuleMain()));
+}
