582d15af459d7a71cabf28ee42de936f22b2398f
shaolonger <shaolong.chuang@qq.com>
2021-01-29 19:18:09 +0800

    fix(web-monitor-ui-flutter): 修复在Android Emulator无法通过localhost访问主机的问题，解决方案是通过10.0.2.2的方式间接访问

--- web-monitor-ui/web-monitor-ui-flutter/lib/config/environment_config.dart
+++ web-monitor-ui/web-monitor-ui-flutter/lib/config/environment_config.dart
@@ -6,6 +6,10 @@ class EnvironmentConfig {
   );
 
   // URL前缀地址
+  // 注意，由于在Android Emulator中无法直接访问主机的localhost，
+  // 因此需要通过访问10.0.2.2的访问间接访问，
+  // 或者，也可以通过在Emulator->Setting中设置Proxy代理来实现
+  // 参考资料：https://stackoverflow.com/questions/5528850/how-do-you-connect-localhost-in-the-android-emulator
   static const String API_URL =
-      ENV == "dev" ? "http://localhost:6001" : "http://localhost:6001";
+      ENV == "dev" ? "http://10.0.2.2:6001" : "http://10.0.2.2:6001";
 }
