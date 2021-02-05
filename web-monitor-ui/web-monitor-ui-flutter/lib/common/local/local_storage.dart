d7eddc539b9704c9c03204ab23123ee870a793ae
shaolonger <shaolong.chuang@qq.com>
2021-02-05 22:35:54 +0800

    feat(web-monitor-ui-flutter): 新增LocalStorage，利用shared_preferences库实现flutter的本地缓存功能

---
+++ web-monitor-ui/web-monitor-ui-flutter/lib/common/local/local_storage.dart
@@ -0,0 +1,23 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// 利用SharedPreferences插件，实现本地存储
+class LocalStorage {
+
+  /// 保存本地缓存
+  static void save(String key, String value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, value);
+  }
+
+  /// 获取本地缓存
+  static Future<String> get(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.get(key);
+  }
+
+  /// 移除本地缓存
+  static void remove(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove(key);
+  }
+}
