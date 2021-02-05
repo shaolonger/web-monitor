35e3d71a0789064ca23f7c1d74885e7d3dfbcff9
shaolonger <shaolong.chuang@qq.com>
2021-02-05 22:35:17 +0800

    feat(web-monitor-ui-flutter): 新增intl库，新增util_date_time用于格式化日期时间

---
+++ web-monitor-ui/web-monitor-ui-flutter/lib/utils/util_date_time.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class UtilDateTime {
+  /// 根据日期格式，返回指定时间的日期字符串
+  static String getDateStrByFormatAndDateTime({
+    String pattern = "yyyy-MM-dd HH:mm:ss",
+    @required DateTime dateTime,
+  }) {
+    return DateFormat(pattern, "zh").format(dateTime);
+  }
+}
