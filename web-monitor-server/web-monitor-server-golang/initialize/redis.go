ae2d7d3d162fab2c74a254eccc57a0b1d53227dc
shaolonger <shaolong.chuang@qq.com>
2021-07-20 23:24:43 +0800

    feat(web-monitor-server-golang): 若redis连接失败，则退出程序

--- web-monitor-server/web-monitor-server-golang/initialize/redis.go
+++ web-monitor-server/web-monitor-server-golang/initialize/redis.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/go-redis/redis/v8"
 	"go.uber.org/zap"
+	"os"
 	"web.monitor.com/global"
 )
 
@@ -19,6 +20,7 @@ func Redis() {
 	pong, err := client.Ping(ctx).Result()
 	if err != nil {
 		global.WM_LOG.Error("redis连接失败, err:", zap.Any("err", err))
+		os.Exit(0)
 	} else {
 		global.WM_LOG.Info("redis连接成功:", zap.String("pong", pong))
 		global.WM_REDIS = &global.Redis{
