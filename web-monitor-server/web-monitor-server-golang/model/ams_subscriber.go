658acd5176fd2aaddf7838627d1758529d457bb1
shaolonger <shaolong.chuang@qq.com>
2021-07-28 19:23:43 +0800

    feat(web-monitor-server-golang): 更新表结构

--- web-monitor-server/web-monitor-server-golang/model/ams_subscriber.go
+++ web-monitor-server/web-monitor-server-golang/model/ams_subscriber.go
@@ -1,13 +1,11 @@
 package model
 
-import "time"
-
 type AmsSubscriber struct {
-	Id          uint64    `json:"id" gorm:"primaryKey;autoIncrement;comment:ID"`
-	AlarmId     uint64    `json:"alarmId" gorm:"not null;comment:预警规则id"`
-	Subscriber  string    `json:"subscriber" gorm:"type:text;comment:报警订阅方，内容为JSON格式，例如[]。1、若订阅方式为钉钉，则内容为钉钉的机器人Webhook的access_token2、若为邮件订阅，则内容为邮件地址"`
-	SchedulerId uint64    `json:"schedulerId" gorm:"not null;comment:定时任务id"`
-	CreateTime  time.Time `json:"createTime" gorm:"not null;comment:创建时间，格式为yyyy-MM-dd HH:mm:ss"`
+	Id         uint64 `json:"id" gorm:"primaryKey;autoIncrement;comment:ID"`
+	AlarmId    uint64 `json:"alarmId" gorm:"not null;comment:预警规则id"`
+	Subscriber string `json:"subscriber" gorm:"type:text;comment:报警订阅方，内容为JSON格式，例如[]。1、若订阅方式为钉钉，则内容为钉钉的机器人Webhook的access_token2、若为邮件订阅，则内容为邮件地址"`
+	IsActive   int8   `json:"isActive" gorm:"not null;type:tinyint(1);comment:是否启用，0-否，1-是"`
+	Category   int8   `json:"category" gorm:"not null;type:tinyint(1);comment:订阅类型，1-钉钉机器人，2-邮箱"`
 }
 
 func (a AmsSubscriber) TableName() string {
--- web-monitor-server/web-monitor-server-golang/model/ams_subscriber_notify_record.go
+++ web-monitor-server/web-monitor-server-golang/model/ams_subscriber_notify_record.go
@@ -1,11 +1,14 @@
 package model
 
+import "time"
+
 type AmsSubscriberNotifyRecord struct {
-	Id            uint64 `json:"id" gorm:"primaryKey;autoIncrement;comment:ID"`
-	AlarmRecordId uint64 `json:"alarmRecordId" gorm:"not null;comment:报警记录id"`
-	SubscriberId  uint64 `json:"subscriberId" gorm:"not null;comment:报警订阅方id"`
-	State         int8   `json:"state" gorm:"not null;type:tinyint(1);comment:通知状态，0-失败，1-成功"`
-	Content       string `json:"content" gorm:"not null;type:text;comment:通知内容"`
+	Id            uint64    `json:"id" gorm:"primaryKey;autoIncrement;comment:ID"`
+	AlarmRecordId uint64    `json:"alarmRecordId" gorm:"not null;comment:报警记录id"`
+	SubscriberId  uint64    `json:"subscriberId" gorm:"not null;comment:报警订阅方id"`
+	State         int8      `json:"state" gorm:"not null;type:tinyint(1);comment:通知状态，0-失败，1-成功"`
+	Content       string    `json:"content" gorm:"not null;type:text;comment:通知内容"`
+	CreateTime    time.Time `json:"createTime" gorm:"not null;comment:创建时间，格式为yyyy-MM-dd HH:mm:ss"`
 }
 
 func (a AmsSubscriberNotifyRecord) TableName() string {
